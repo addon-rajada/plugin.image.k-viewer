3c69d8fa23be90beb3f6e2e5e28bb7a215a51858
addon-rajada <addon.rajada@proton.me>
2024-03-15 14:55:38 -0300

    fix generate script

--- generate.sh
+++ generate.sh
@@ -1,4 +1,5 @@
 cd ..
 zip -r -u -9 -T plugin.image.k-viewer.zip plugin.image.k-viewer/ \
 -x "plugin.image.k-viewer/.git/*" \
--x "plugin.image.k-viewer/resources/lib/check/*"
+-x "plugin.image.k-viewer/resources/lib/check/*" \
+-x "plugin.image.k-viewer/*/__pycache__/*"
